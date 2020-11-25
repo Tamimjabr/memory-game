dcd1fe327400d7064eb7449f6cde918199497402
Tamim Jabr <tj222kg@student.lnu.se>
2020-11-25 12:55:05 +0100

    create the tiles in the game board

--- src/components/my-memory-game/my-memory-game.js
+++ src/components/my-memory-game/my-memory-game.js
@@ -33,6 +33,10 @@ template.innerHTML = `
     grid-template-columns: repeat(4,var(--tile-size));
     gap: 20px;
     }
+    /*if it is small game so it's enough with two columns*/
+    #game-board.small{
+      grid-template-columns: repeat(2,var(--tile-size))
+    }
     my-flipping-tile{
       width:var(--tile-size);
       height:var(--tile-size);
@@ -51,9 +55,7 @@ template.innerHTML = `
     </my-flipping-tile>
   </template>
   <div id='game-board'>
-  <my-flipping-tile>
-  
-    </my-flipping-tile>
+
   </div>
 
 `
@@ -112,7 +114,10 @@ customElements.define('my-memory-game',
      * Called after the element is inserted into the DOM.
      */
     connectedCallback () {
-
+      if (!this.hasAttribute('boardsize')) {
+        this.setAttribute('boardsize', 'large')
+      }
+      this._upgradeProperty('boardsize')
     }
 
     /**
@@ -123,7 +128,9 @@ customElements.define('my-memory-game',
      * @param {*} newValue - The new value.
      */
     attributeChangedCallback (name, oldValue, newValue) {
-
+      if (name === 'boardsize') {
+        this._init()
+      }
     }
 
     /**
@@ -139,17 +146,19 @@ customElements.define('my-memory-game',
      * @returns {object} The width and height of the game board.
      */
     get _gameBoardSize () {
-      // the default size of the game board if there is no boardsize attribute given
+      // the default size of the game board is 4x4 if there is no boardsize attribute given
       const gameBoardSize = {
         width: 4,
         height: 4
       }
-      // this.getAttribute('boardsize') = this.boardSize
-      switch (this.getAttribute('boardsize')) {
+      // !this.getAttribute('boardsize') = this.boardSize
+      switch (this.boardSize) {
+        // when small make the game 2x2
         case 'small':
           gameBoardSize.width = gameBoardSize.height = 2
           break
         case 'medium' : {
+          // when medium make the game 4x2
           gameBoardSize.height = 2
           break
         }
@@ -183,6 +192,61 @@ customElements.define('my-memory-game',
         delete this[prop]
         this[prop] = value
       }
+    }
+
+    /**
+     * Initializes the game board size and tiles.
+     */
+    _init () {
+      const { width, height } = this._gameBoardSize
+      // the number of the tiles ex. 4*4=16
+      const tilesCount = width * height
+      console.log(tilesCount)
+
+      // if the new number of tiles given not equal the existing one
+      if (tilesCount !== this._tiles.all.length) {
+        // remove the tiles from the board
+        this._gameBoard.innerHTML = ''
+      }
+
+      // add class small if the width ===2, means two columns
+      if (width === 2) {
+        this._gameBoard.classList.add('small')
+      } else {
+        this._gameBoard.classList.remove('small')
+      }
+
+      // add tiles to the  game board, but they are only with back face, we have to add images to front face at next step
+      for (let i = 0; i < tilesCount; i++) {
+        this._gameBoard.appendChild(this._tileTemplate.content.cloneNode(true))
+      }
+
+      // Create a sequence of numbers between 0 and 15,
+      // and then shuffle the sequence.
+      // get indexes from 0 to 15 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+      const indexes = [...Array(tilesCount).keys()]
+
+      console.log(indexes)
+      // shuffle the indexes ex. [4, 12, 8, 5, 7, 14, 15, 10, 6, 13, 3, 0, 11, 1, 9, 2]
+
+      for (let i = indexes.length - 1; i > 0; i--) {
+        // random a number between 0 and i
+        const j = Math.floor(Math.random() * (i + 1))
+        const temp = indexes[j]
+        indexes[j] = indexes[i]
+        indexes[i] = temp
+      }
+      console.log(indexes)
+
+      this._tiles.all.forEach((tile, index) => {
+        // ! % is the rest value not division , ex 3%8+1= 4 because 3%8 =3
+        // debugging const fi = indexes[index] % (tilesCount / 2) + 1
+      // console.log(indexes[index],(tilesCount / 2))
+        tile.querySelector('img').setAttribute('src', IMG_URLS[indexes[index] % (tilesCount / 2) + 1])
+        // [indexes[index]%(this._tiles/2)+1] result twins of numbers between 1 and 8
+        // todo check if necessary
+        tile.faceUp = tile.disabled = tile.hidden = false
+      })
     }
   }
 )
